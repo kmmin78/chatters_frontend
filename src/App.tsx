222e43a31bde40217c676849c94f75f68ddcdb79
kms <kmmin78@naver.com>
2020-08-12 03:03:41 +0900

    2020-08-12 3:03  - login form 구현  - react-spring을 이용해서 애니메이션 구현해보자  - 그 후에 login 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Login } from 'components/pages';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom';
+import { Login } from './components/pages';
 
-function App(props) {
+function App<T>(props: T) {
     return (
         <>
             <Router>
                 <Switch>
                     <Route exact path={['/']} component={Login}></Route>
+                    <Redirect path='*' to='/' />
                 </Switch>
             </Router>
         </>
